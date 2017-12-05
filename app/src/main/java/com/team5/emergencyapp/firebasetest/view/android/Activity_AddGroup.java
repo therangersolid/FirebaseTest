d1a8dc18b306ee316f57ccdbe7145ff74dea4f6e
therangersolid <therangersolid@gmail.com>
2017-12-04 19:23:19 -0800

    With a good UI

--- app/src/main/java/com/team5/emergencyapp/firebasetest/view/android/Activity_AddGroup.java
+++ app/src/main/java/com/team5/emergencyapp/firebasetest/view/android/Activity_AddGroup.java
@@ -19,47 +19,53 @@ import com.team5.emergencyapp.firebasetest.firebase.dao.DUserList;
 import java.util.ArrayList;
 import java.util.Set;
 
-public class Activity_Contact extends AppCompatActivity {
-    public static final String MESSAGE = "com.team5.emergencyapp.MESSAGE"; // The key of the message
-    public static final String MESSAGE_TYPE = "com.team5.emergencyapp.MESSAGE_TYPE"; // the data will be broadcast or individual or bot
-    public static final String BROADCAST = "BROADCAST"; // the data will be broadcast or individual or bots
-    public static final String INDIVIDUAL = "INDIVIDUAL"; // the data will be broadcast or individual or bots
-    public static final String BOT = "BOT"; // the data will be broadcast or individual or bots
-    public static final String ADDGROUP = "ADDGROUP"; // Window to add the group to Activity_AddGroup
-    private Activity_Contact activityContact;
+public class Activity_AddGroup extends AppCompatActivity {
 
-    public void setContent(final String messageType, String displayName, final String key, LinearLayout messageList) {
-        LinearLayout linearLayout = new LinearLayout(activityContact);
+    class ListType {
+        String id;
+        boolean selected = false;
+        boolean broadcast;
+    }
+
+    private Activity_AddGroup activityAddGroup;
+
+
+    public void setContent(final String messageType, String displayName, final String key, LinearLayout messageList, ArrayList<ListType> listTypes) {
+        final LinearLayout linearLayout = new LinearLayout(activityAddGroup);
+        final ListType listType = new ListType();
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        ImageView imageView = new ImageView(activityContact);
-        if (messageType.equals(BROADCAST) || messageType.equals(BOT)) {
+        ImageView imageView = new ImageView(activityAddGroup);
+        if (messageType.equals(Activity_Contact.BROADCAST) || messageType.equals(Activity_Contact.BOT)) {
             imageView.setBackgroundResource(R.drawable.admin_man);
-        } else if(messageType.equals(INDIVIDUAL)){
+            listType.broadcast = true;
+        } else {
             imageView.setBackgroundResource(R.drawable.man);
+            listType.broadcast = false;
         }
         linearLayout.addView(imageView);
-        TextView tempTextView = new TextView(activityContact);
+        TextView tempTextView = new TextView(activityAddGroup);
         tempTextView.setText(displayName);
+        listType.id = key;
         tempTextView.setPadding(Utility.dipToPX(10), Utility.dipToPX(3), 0, 0);
         tempTextView.setTextColor(getResources().getColor(R.color.black));
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (messageType.equals(ADDGROUP)){
-                    Intent intent = new Intent(activityContact, Activity_AddGroup.class);
-                    activityContact.startActivity(intent);
-                }else {
-                    Intent intent = new Intent(activityContact, Activity_Message.class);
-                    intent.putExtra(MESSAGE_TYPE, messageType);
-                    intent.putExtra(MESSAGE, key);
-                    activityContact.startActivity(intent);
+                if (!listType.selected) {
+                    linearLayout.setBackgroundColor(Color.GREEN);
+                    listType.selected = true;
+                } else {
+                    linearLayout.setBackgroundColor(Color.TRANSPARENT);
+                    listType.selected = false;
                 }
+
             }
         });
         linearLayout.addView(tempTextView);
         linearLayout.setPadding(0, Utility.dipToPX(3), 0, Utility.dipToPX(10));
         messageList.addView(linearLayout);
-        LinearLayout lineLinearLayout = new LinearLayout(activityContact);
+        listTypes.add(listType);
+        LinearLayout lineLinearLayout = new LinearLayout(activityAddGroup);
         lineLinearLayout.setPadding(0, Utility.dipToPX(1), 0, 0);
         lineLinearLayout.setBackgroundColor(Color.GRAY);
         messageList.addView(lineLinearLayout);
@@ -68,12 +74,11 @@ public class Activity_Contact extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        setContentView(R.layout.activity_addgroup);
 
-        activityContact = this;
+        activityAddGroup = this;
         final LinearLayout messageList = (LinearLayout) findViewById(R.id.groupList);
-        setContent(ADDGROUP, "Add or delete a group!", "", messageList);
-        setContent(BOT, "Chat with bot!", "", messageList);
+        final ArrayList<ListType> listTypes = new ArrayList<ListType>();
         Thread t = new Thread(new Runnable() { // get the user group first!
             @Override
             public void run() {
@@ -92,7 +97,7 @@ public class Activity_Contact extends AppCompatActivity {
                     public void run() {
                         Set<String> keysets = us.getBroadcast().keySet();
                         for (final String key : keysets) {
-                            setContent(BROADCAST, key, key, messageList);
+                            setContent(Activity_Contact.BROADCAST, key, key, messageList, listTypes);
                             Log.e("Test", key);
                         }
                     }
@@ -110,7 +115,7 @@ public class Activity_Contact extends AppCompatActivity {
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        setContent(INDIVIDUAL, fuser.getEmail(), userID, messageList);
+                                        setContent(Activity_Contact.INDIVIDUAL, fuser.getEmail(), userID, messageList,listTypes);
                                     }
                                 });
                                 Log.e("Test", users3.get(i).getId());
@@ -124,6 +129,19 @@ public class Activity_Contact extends AppCompatActivity {
             }
         });
         t.start();
+
+        // Add and remove group
+        TextView addGroup = (TextView) findViewById(R.id.addgroup);
+        addGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int size = listTypes.size();
+                for (int i = 0; i < size; i++){
+                    // check here
+                }
+            }
+        });
+        TextView removeGroup = (TextView) findViewById(R.id.removegroup);
 
     }
 }
---
+++ app/src/main/java/com/team5/emergencyapp/firebasetest/view/android/Activity_SoundButton.java
@@ -0,0 +1,36 @@
+package com.team5.emergencyapp.firebasetest.view.android;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.team5.emergencyapp.firebasetest.R;
+
+public class Activity_SoundButton extends AppCompatActivity {
+    boolean started = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundbutton);
+
+        final MediaPlayer catSoundMediaPlayer = MediaPlayer.create(this, R.raw.cat_sound);
+
+        final Button playCatMeow = (Button) this.findViewById(R.id.play_cat_meow);
+
+        playCatMeow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!started) {
+                    catSoundMediaPlayer.start();
+                    started = true;
+                }else{
+                    catSoundMediaPlayer.stop();
+                    started = false;
+                }
+            }
+        });
+
+
+    }
+}
